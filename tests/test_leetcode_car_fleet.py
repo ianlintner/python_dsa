a35af54ee417cf45817dc77750d62313847f2203
Ian Lintner <lintner.ian@gmail.com>
2025-08-30 02:06:00 -0500

    Fix syntax error in car fleet test speeds initialization
    Fix invalid unpacking syntax `*10**5` to proper list creation `[10**5] * 10**5` in the performance test case.

--- tests/test_leetcode_car_fleet.py
+++ tests/test_leetcode_car_fleet.py
@@ -25,5 +25,5 @@ class TestSolution:
         solution = Solution()
         target = 10**6
         positions = list(range(0, 10**5))
-        speeds = *10**5
+        speeds = [10**5] * 10**5
         assert solution.solve(target, positions, speeds) == 10**5
